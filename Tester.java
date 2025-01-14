2a18c9156c486c2e0533822d1dd25c7df058c477
Nowar <nowarnizum2008@icloud.com>
2025-01-14 09:45:01 -0500

    SnowGolem tested for bugs

--- Tester.java
+++ Tester.java
@@ -1,5 +1,23 @@
 public class Tester{
   public static void main(String[] args){
-    
+    Adventurer Snow = new SnowGolem();
+    Adventurer CW = new CodeWarrior();
+
+    System.out.println(Snow.getName() + ", " + Snow.getSpecialName() + ", " + Snow.getSpecial() + ", " + Snow.getSpecialMax());
+    Snow.setSpecial(29);
+    System.out.println(Snow.getName() + ", " + Snow.getSpecialName() + ", " + Snow.getSpecial() + ", " + Snow.getSpecialMax());
+
+    System.out.println(CW.attack(Snow));
+    System.out.println(Snow + ", " + Snow.getHP() + "/" + Snow.getmaxHP() + " HP, " + Snow.getSpecial() + "/" + Snow.getSpecialMax() + " " + Snow.getSpecialName());
+    System.out.println(Snow.attack(CW));
+    System.out.println(CW + ", " + CW.getHP() + "/" + CW.getmaxHP() + " HP, " + CW.getSpecial() + "/" + CW.getSpecialMax() + " " + CW.getSpecialName());
+    System.out.println(Snow.support(CW));
+    System.out.println(CW.support(Snow));
+
+    System.out.println(Snow + ", " + Snow.getHP() + "/" + Snow.getmaxHP() + " HP, " + Snow.getSpecial() + "/" + Snow.getSpecialMax() + " " + Snow.getSpecialName());
+    System.out.println(CW + ", " + CW.getHP() + "/" + CW.getmaxHP() + " HP, " + CW.getSpecial() + "/" + CW.getSpecialMax() + " " + CW.getSpecialName());
+    System.out.println(Snow.specialAttack(CW));
+    System.out.println(CW + ", " + CW.getHP() + "/" + CW.getmaxHP() + " HP, " + CW.getSpecial() + "/" + CW.getSpecialMax() + " " + CW.getSpecialName());
+
   }
 }
